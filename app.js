058a6539827be97656ad8e68a0bc5b65527a097d
bin-muhammad <binatiqmuhammad26@gmal.com>
2023-08-21 15:40:34 -0400

    don wark

---
+++ app.js
@@ -0,0 +1,49 @@
+
+// // //var userinput = + prompt("ENTER value 1" )
+// // var userinput2 = + prompt("enter value 2")
+// // var res = userinput + userinput2
+// // document.write(res)
+
+// // //var email = prompt ('enter email');
+// // var password = prompt ('enter password');
+
+// // if(email== 'muhammad' && password == '123456'){
+// //     document.write('login') 
+// // }
+// // else {
+// //     document.write('invalid user name password')
+// // }
+
+// var arr = [1,2,3,4,5]
+// var arr2 = arr.slice ([0])
+// console.log(arr2)
+
+// var i;
+// for(i=1; i<=5; i++){
+//     document.write('muhammad bin atiq <br>')
+// }
+function calculation(value){
+     let textFiled = document.getElementById("calcuator-text");
+    textFiled.value = textFiled.value + value;  
+}
+
+function clearInput() {
+    let textFiled = document.getElementById("calcuator-text");
+    textFiled.value = '' ;  
+}
+
+function result() {
+
+    let textFiled = document.getElementById("calcuator-text");
+    textFiled.value = eval(textFiled.value)
+}
+
+function mn(){
+    var a = slic
+    
+    
+}    
+
+
+ 
+
